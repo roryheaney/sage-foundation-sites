4980ceef53995a8dac0626bb269a2f2820904bb4
Rory Heaney <roryheaney@users.noreply.github.com>
2016-04-09 17:59:00 -0700

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,25 @@
-<header>
-  <div class="title-bar" data-responsive-toggle="top-menu" data-hide-for="medium">
-    <button class="menu-icon" type="button" data-toggle></button>
-    <div class="title-bar-title">Menu</div>
-  </div>
-  <div class="top-bar" id="top-menu">
-    <div class="top-bar-left">
-        <ul class="menu">
-          <li class="menu-text"><?php bloginfo('name'); ?></li>
-        </ul>
-    </div>
+<header id="site-header">
+  <div class="top-bar" id="top-menu" data-topbar role="navigation">
+    <ul class="title-area">
+      <li class="name"><h1><a href="<?php echo esc_url(home_url()); ?>">
+        <?php bloginfo('name'); ?>
+      </a></h1> </li>
+      <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+    </ul>
     <div class="top-bar-right">
       <ul class="dropdown menu" data-dropdown-menu>
         <?php if (has_nav_menu('primary_navigation')) :?>
-          <?php wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => '', 'items_wrap' => '%3$s', 'walker' => new Roots\Sage\Extras\Foundation_Nav_Menu()]);?>
+            <?php wp_nav_menu([
+              'theme_location' => 'primary_navigation', 
+              'menu_class' => 'nav', 
+              'container' => '', 
+              'items_wrap' => '%3$s', 
+              'walker' => new Roots\Sage\Extras\Foundation_Nav_Menu()
+            ]);?>
         <?php endif;?>
       </ul>
     </div>
+
+ 
   </div>
 </header>
