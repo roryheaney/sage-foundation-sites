806ebad05e6d95567c664bf8ebeeccd11f773b2e
Rory Heaney <roryheaney@users.noreply.github.com>
2016-06-30 21:23:15 -0700

    update test sample
    sample test updated

--- assets/scripts/components/sample.js
+++ assets/scripts/components/sample.js
@@ -1,1 +1,20 @@
 //remove me
+//remove me
+var Testing = (function ($) {
+
+	
+
+    function init() {
+
+    	console.log('test ha');
+    }
+
+    
+
+
+    return {
+        init: init
+    };
+    
+
+})(jQuery);
